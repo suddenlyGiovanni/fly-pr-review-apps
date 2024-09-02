4dde09689bf227388afc183dafdd14afe151a873
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-09-02 17:41:55 +0200

    Add detailed comments for Fly app configuration and deployment
    Enhanced the entrypoint.sh script by adding detailed comments explaining the Fly app configuration and deployment commands. This includes descriptions for options like region, org, and image, among others, improving readability and maintainability.

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,13 +18,26 @@ EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 
 # Default the Fly app name to pr-{number}-{repo_owner}-{repo_name}
 app="${INPUT_NAME:-pr-$PR_NUMBER-$GITHUB_REPOSITORY_OWNER-$GITHUB_REPOSITORY_NAME}"
+
 # Change underscores to hyphens.
 app="${app//_/-}"
+
+# --region string                    The target region (see 'flyctl platform regions')
 region="${INPUT_REGION:-${FLY_REGION:-iad}}"
+
+# --org string                       The target Fly.io organization
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
+
+# --image string                     The Docker image to deploy
 image="$INPUT_IMAGE"
+
+# --config string                    Path to application configuration file
 config="${INPUT_CONFIG:-fly.toml}"
+
+# --build-arg stringArray            Set of build time variables in the form of NAME=VALUE pairs.
 build_args=""
+
+# --build-secret stringArray         Set of build secrets of NAME=VALUE pairs.
 build_secrets=""
 
 if ! echo "$app" | grep "$PR_NUMBER"; then
@@ -34,6 +47,9 @@ fi
 
 # PR was closed - remove the Fly app if one exists and exit.
 if [ "$EVENT_TYPE" = "closed" ]; then
+  # Manage your Fly applications.
+  # destroy = Delete one or more applications from the Fly platform.
+  #         -y, --yes = Accept all confirmations
   flyctl apps destroy "$app" -y || true
   exit 0
 fi
@@ -54,6 +70,15 @@ fi
 if ! flyctl status --app "$app"; then
   # Backup the original config file since 'flyctl launch' messes up the [build.args] section
   cp "$config" "$config.bak"
+  # Create and configure a new app from source code or a Docker image.
+  # --no-deploy = Do not immediately deploy the new app after fly launch creates and configures it
+  # --copy-config = Use the configuration file if present without prompting
+  # --name = Name of the new app
+  # --image = The Docker image to deploy
+  # --region = The target region (see 'flyctl platform regions')
+  # --org = The target Fly.io organization
+  # --build-arg = Set of build time variables in the form of NAME=VALUE pairs. Can be specified multiple times.
+  # --build-secret = Set of build secrets of NAME=VALUE pairs. Can be specified multiple times.
   flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org" ${build_args} ${build_secrets}
   # Restore the original config file
   cp "$config.bak" "$config"
@@ -70,6 +95,18 @@ fi
 
 # Trigger the deploy of the new version.
 echo "Contents of config $config file: " && cat "$config"
+# Deploy Fly applications from source or an image using a local or remote builder.
+# --config = Path to application configuration file
+# --app = application name
+# --regions = Deploy to machines only in these regions.
+# --image = The Docker image to deploy
+# --strategy = The strategy for replacing running instances. Options are canary, rolling, bluegreen, or immediate. The default strategy is rolling.
+# --ha = Create spare machines that increases app availability (default true)
+# --vm-size = The VM size to set machines to. See "fly platform vm-sizes" for valid values
+# --vm-cpu-kind = The kind of CPU to use ('shared' or 'performance')
+# --vm-cpus = Number of CPUs
+# --vm-memory = Memory (in megabytes) to attribute to the VM
+
 if [ -n "$INPUT_VM" ]; then
   flyctl deploy --config "$config" --app "$app" --regions "$region" --image "$image" --strategy immediate --ha=$INPUT_HA ${build_args} ${build_secrets} --vm-size "$INPUT_VMSIZE"
 else
@@ -77,7 +114,7 @@ else
 fi
 
 # Make some info available to the GitHub workflow.
-flyctl status --app "$app" --json >status.json
+flyctl status --app "$app" --json > status.json
 hostname=$(jq -r .Hostname status.json)
 appid=$(jq -r .ID status.json)
 echo "hostname=$hostname" >> $GITHUB_OUTPUT
